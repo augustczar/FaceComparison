5195414879eb513bf9c6e81fc028a836c3a539a2
augustczar <augustczarramos@gmail.com>
2023-12-20 15:26:37 -0300

    creating implements and add rekognition, changing to receive base64 and returning the highest result

--- facecomparison/src/main/java/com/augustczar/hub/facecomparison/FacecomparisonApplication.java
+++ facecomparison/src/main/java/com/augustczar/hub/facecomparison/FacecomparisonApplication.java
@@ -2,11 +2,19 @@ package com.augustczar.hub.facecomparison;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import software.amazon.awssdk.services.rekognition.RekognitionClient;
 
 @SpringBootApplication
 public class FacecomparisonApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FacecomparisonApplication.class, args);
+	}
+	
+	@Bean
+	public RekognitionClient rekognitionClient() {
+		return RekognitionClient.create();
 	}
 }
--- facecomparison/src/main/java/com/augustczar/hub/facecomparison/service/impl/ComparisonServiceImpl.java
+++ facecomparison/src/main/java/com/augustczar/hub/facecomparison/service/impl/ComparisonServiceImpl.java
@@ -1,14 +1,79 @@
 package com.augustczar.hub.facecomparison.service.impl;
 
+import java.security.InvalidParameterException;
+import java.util.Base64;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.augustczar.hub.facecomparison.service.ComparisonService;
 
+import software.amazon.awssdk.core.SdkBytes;
+import software.amazon.awssdk.services.rekognition.RekognitionClient;
+import software.amazon.awssdk.services.rekognition.model.BoundingBox;
+import software.amazon.awssdk.services.rekognition.model.CompareFacesMatch;
+import software.amazon.awssdk.services.rekognition.model.CompareFacesRequest;
+import software.amazon.awssdk.services.rekognition.model.CompareFacesResponse;
+import software.amazon.awssdk.services.rekognition.model.ComparedFace;
+import software.amazon.awssdk.services.rekognition.model.Image;
+/**
+ * https://docs.aws.amazon.com/sdk-for-java/latest/developer-guide/java_rekognition_code_examples.html
+ */
 @Service
-public class ComparisonServiceImpl implements ComparisonService{
+public class ComparisonServiceImpl implements ComparisonService {
+
+	@Autowired
+	private RekognitionClient rekognitionClient;
+
+	 Float similarityThreshold = 70F;
+	 
 	@Override
 	public float compareTwoPhotos(String photo1, String photo2) {
-		return 0.0f;
-	}
+		float result = 0f;
+		try {
+			SdkBytes sourceBytes = SdkBytes
+					.fromByteArray(Base64.getDecoder()
+							.decode(photo1.substring(photo1.indexOf(',') + 1)));
+			SdkBytes targetBytes = SdkBytes
+					.fromByteArray(Base64.getDecoder()
+							.decode(photo2.substring(photo2.indexOf(',') + 1)));
 
+			// Create an Image object for the source image.
+			Image souImage = Image.builder().bytes(sourceBytes).build();
+
+			Image tarImage = Image.builder().bytes(targetBytes).build();
+
+			CompareFacesRequest facesRequest = CompareFacesRequest.builder()
+					.sourceImage(souImage)
+					.targetImage(tarImage)
+					.similarityThreshold(similarityThreshold)
+					.build();
+
+			// Compare the two images.
+			CompareFacesResponse compareFacesResult = rekognitionClient.compareFaces(facesRequest);
+			List<CompareFacesMatch> faceDetails = compareFacesResult.faceMatches();
+			for (CompareFacesMatch match : faceDetails) {
+				ComparedFace face = match.face();
+				BoundingBox position = face.boundingBox();
+				System.out.println("Face at " + position.left().toString() + " " + position.top() + " matches with "
+						+ face.confidence().toString() + "% confidence.");
+
+			}
+			List<ComparedFace> uncompared = compareFacesResult.unmatchedFaces();
+			System.out.println("There was " + uncompared.size() + " face(s) that did not match");
+			System.out.println("Source image rotation: " + compareFacesResult.sourceImageOrientationCorrection());
+			System.out.println("target image rotation: " + compareFacesResult.targetImageOrientationCorrection());
+
+            // Separa o MAIOR RESULTADO!
+            Optional<CompareFacesMatch> resultMatchFaceMatch = faceDetails.stream().max(Comparator.comparing(a -> a.similarity().floatValue()));
+            result = resultMatchFaceMatch.isPresent() ? resultMatchFaceMatch.get().similarity().floatValue() : 0f;
+            
+		} catch (InvalidParameterException e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
 }
