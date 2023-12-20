12b11d919368bb0d5d342dec0ac876f543f20170
augustczar <augustczarramos@gmail.com>
2023-12-20 15:21:27 -0300

    creating service and controller

--- facecomparison/src/main/java/com/augustczar/hub/facecomparison/controller/ComparisonController.java
+++ facecomparison/src/main/java/com/augustczar/hub/facecomparison/controller/ComparisonController.java
@@ -3,24 +3,27 @@ package com.augustczar.hub.facecomparison.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.augustczar.hub.facecomparison.service.ComparisonService;
 
 @RestController
-@RequestMapping(path = "facecomparison", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping("/facecomparison")
 public class ComparisonController {
 	
 	@Autowired
 	private ComparisonService comparisonService;
 
-	@GetMapping("status")
+	@GetMapping(path = "/status", produces = MediaType.APPLICATION_JSON_VALUE)
 	public Status getStatus() {
 		return new Status("OK And Running!");
 	}
 	
-	public Output compare(Input input) throws Exception{
+	@PostMapping(path = "/compare", consumes = MediaType.APPLICATION_JSON_VALUE)
+	public Output compare(@RequestBody Input input) throws Exception{
 		try {
 			float result = comparisonService.compareTwoPhotos(input.photo1(), input.photo2());
 			return new Output(result);
--- facecomparison/src/main/java/com/augustczar/hub/facecomparison/service/ComparisonService.java
+++ facecomparison/src/main/java/com/augustczar/hub/facecomparison/service/ComparisonService.java
@@ -2,6 +2,5 @@ package com.augustczar.hub.facecomparison.service;
 
 public interface ComparisonService {
 
-	float compareTwoPhotos(String photo1, String photo2);
-
+	 float compareTwoPhotos(String photo1, String photo2);
 }
