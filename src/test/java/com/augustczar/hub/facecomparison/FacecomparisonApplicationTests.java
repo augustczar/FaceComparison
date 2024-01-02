d7af935f574d21000038af0010935936a9ab0024
augustczar <augustczarramos@gmail.com>
2024-01-02 15:59:46 -0300

    test update

--- src/test/java/com/augustczar/hub/facecomparison/FacecomparisonApplicationTests.java
+++ src/test/java/com/augustczar/hub/facecomparison/FacecomparisonApplicationTests.java
@@ -1,13 +1,21 @@
 package com.augustczar.hub.facecomparison;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(classes = FacecomparisonApplication.class)
 @ActiveProfiles(value = "test")
 class FacecomparisonApplicationTests {
 
+	@BeforeAll
+	static void setupProperties() {
+		System.setProperty("aws.region", "us-east-1");
+		System.setProperty("aws.accessKeyId", "localstack");
+		System.setProperty("aws.secretAccessKey", "localstack");
+	}
+	
 	@Test
 	void contextLoads() {
 	}
