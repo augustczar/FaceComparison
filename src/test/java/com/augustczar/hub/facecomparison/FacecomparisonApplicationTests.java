073f08777d488d69f488eae470fd3035776fc216
augustczar <augustczarramos@gmail.com>
2024-01-02 15:38:33 -0300

    update test

--- src/test/java/com/augustczar/hub/facecomparison/FacecomparisonApplicationTests.java
+++ src/test/java/com/augustczar/hub/facecomparison/FacecomparisonApplicationTests.java
@@ -2,8 +2,10 @@ package com.augustczar.hub.facecomparison;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles(value = "test")
 class FacecomparisonApplicationTests {
 
 	@Test
